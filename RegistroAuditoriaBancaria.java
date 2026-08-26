6e4c6fd720753ff520e5f2249de1d5090e72d949
dimalancheros <di.ma.lancheros@unillanos.edu.co>
2026-08-26 17:43:25 -0500

    Create RegistroAuditoriaBancaria.java

---
+++ RegistroAuditoriaBancaria.java
@@ -0,0 +1,18 @@
+public class RegistroAuditoriaBancaria implements AutoCloseable {
+
+    private String idRegistro;
+
+    public RegistroAuditoriaBancaria(String idRegistro) {
+        this.idRegistro = idRegistro;
+        System.out.println("[AUDITORÍA] Abriendo recurso de log para el registro: " + this.idRegistro);
+    }
+
+    public void registrarEvento(String mensaje) {
+        System.out.println("[LOG - " + idRegistro + "]: " + mensaje);
+    }
+
+    @Override
+    public void close() {
+        System.out.println("[AUDITORÍA] Cerrando y liberando recurso de forma segura para el registro: " + this.idRegistro);
+    }
+}
