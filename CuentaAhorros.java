4721a1a3a07a7b0dc5db3feb653bd115beae992b
dimalancheros <di.ma.lancheros@unillanos.edu.co>
2026-08-26 17:31:09 -0500

    Update CuentaAhorros.java

--- CuentaAhorros.java
+++ CuentaAhorros.java
@@ -1,1 +1,38 @@
+public class CuentaAhorros extends CuentaBancaria {
+    
+    private double tasaInteres;
+    private static final double COMISION_FIJA = 15.0; 
 
+    public CuentaAhorros(String numeroCuenta, String titular, double saldoInicial, double tasaInteres) {
+        super(numeroCuenta, titular, saldoInicial);
+        this.tasaInteres = tasaInteres;
+    }
+
+    @Override //no olvidar esto es pa polimorfismo
+    public void retirar(double monto) {
+        if (monto > 0 && (getSaldo() - monto) >= 0) {
+            setSaldo(getSaldo() - monto);
+            System.out.println("Retiro exitoso en Cuenta de Ahorros. Nuevo saldo: $" + getSaldo());
+        } else {
+            System.out.println("Fondos insuficientes o monto inválido. Las cuentas de ahorros no permiten sobregiros.");
+        }
+    }
+
+    @Override
+    public void aplicarComisionMensual() {
+        if (getSaldo() >= COMISION_FIJA) {
+            setSaldo(getSaldo() - COMISION_FIJA);
+            System.out.println("Comisión mensual aplicada. Saldo actual: $" + getSaldo());
+        } else {
+            System.out.println("Saldo insuficiente para aplicar la comisión de manejo.");
+        }
+    }
+
+    public double getTasaInteres() {
+        return tasaInteres;
+    }
+
+    public void setTasaInteres(double tasaInteres) {
+        this.tasaInteres = tasaInteres;
+    }
+}
